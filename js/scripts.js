e0d5ad798def5787b6c02f299f20820447819ed0
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-17 10:05:21 -0700

    Make update function and pass language, prependImage and appendMessage as arguments.

--- js/scripts.js
+++ js/scripts.js
@@ -27,50 +27,61 @@ $(document).ready(function(){
     let name = $("#name").val();
 
     showResultPage(name);
-    
-    // Prepare results for when the form submits
-    // Default will assume result === 5, aka C#
-    let language = "C#";
-    let prependImage = '<img src="img/c-sharp.png" alt="C# logo" class="logos">';
-    let appendMessage = "<p>You can learn how to make games and VR!</p>";
-    let results = city + project + dogOrCat + environment + size;
 
-    if (results === 6) {
-      language = "Ruby";
-      prependImage = '<img src="img/ruby.png" alt="Ruby logo" class="logos">';
-      appendMessage = "<p>You can learn how to make applications!</p>";
+    const update = (language, prependImage, appendMessage) => {
+      $("#your-language").text(language);
+      $(".result-message").prepend(prependImage).append(appendMessage);
+    };
+    let results = city + project + dogOrCat + environment + size;
+    if (results === 5) {
+      update(
+        "C#",
+        '<img src="img/c-sharp.png" alt="C# logo" class="logos">',
+        "<p>You can learn how to make games and VR!</p>",
+      );
+    } else if (results === 6) {
+      update(
+        "Ruby",
+        '<img src="img/ruby.png" alt="Ruby logo" class="logos">',
+        "<p>You can learn how to make applications!</p>",
+      );
     } else if (results === 7) {
-      language = 'JavaScript';
-      prependImage = '<img src="img/javascript.jpg" alt="JavaScript logo" class="logos">'
-      appendMessage = "<p>You can learn how to make cool web applications and games!</p>"
+      update(
+        'JavaScript',
+        '<img src="img/javascript.jpg" alt="JavaScript logo" class="logos">',
+        "<p>You can learn how to make cool web applications and games!</p>",
+      );
     } else if (results === 8) {
-      language = "Swift";
-      prependImage = '<img src="img/swift.png" alt="Swift logo" class="logos">';
-      appendMessage = "<p>You can learn how to make applications for MacOS, iOS, watchOS and tvOS!</p>";
+      update(
+        "Swift",
+        '<img src="img/swift.png" alt="Swift logo" class="logos">',
+        "<p>You can learn how to make applications for MacOS, iOS, watchOS and tvOS!</p>",
+      );
     } else if (results === 9) {
-      language = "Go";
-      prependImage = '<img src="img/golang.png" alt="Golang logo" class="logos">';
-      appendMessage = "<p>You can learn efficient, fast then beautiful and straight codes!</p>";
+      update(
+        "Go",
+        '<img src="img/golang.png" alt="Golang logo" class="logos">',
+        "<p>You can learn efficient, fast then beautiful and straight codes!</p>",
+      );
     } else if (results === 10) {
-      language = "Python";
-      prependImage = '<img src="img/python.png" alt="Python logo" class="logos">';
-      appendMessage = "<p>You can learn how to make self-driving car!</p>";
+      update(
+        "Python",
+        '<img src="img/python.png" alt="Python logo" class="logos">',
+        "<p>You can learn how to make self-driving car!</p>",
+      );
     } else {
-      // alert("Please select everything to see the result!");
       $(".modal").modal().fadeIn('fast');
       $(".close").click(function() {
       $(".modal").modal('hide');
       });
       resetMainPage();
     }
-      // Update the page with
-      $("#your-language").text(language);
-      $(".result-message").prepend(prependImage).append(appendMessage);
-      return
+
   });
   $("#reset-btn").click(function(){
     $("input:radio").prop("checked", false);
     resetMainPage();
+    // $('form').trigger('reset');
   })
 });
 
